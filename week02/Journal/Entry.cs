460357b5196c2cfa526570e751b9d18dbe7c0d80
kevin flores <kevinfloresg1998@gmail.com>
2025-07-11 00:13:10 -0300

    Resume done, Journal in teams done 40%

---
+++ week02/Journal/Entry.cs
@@ -0,0 +1,13 @@
+public class Entry
+{
+    public string Date { get; set; }
+    public string Prompt { get; set; }
+    public string Response { get; set; }
+    public Entry(string prompt, string response)
+    {
+        Date = DateTime.Now.ToString("yyyy-MM-dd");
+        Prompt = prompt;
+        Response = response;
+    }
+}
+ 
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
+
+        // We have to do 3 Classes + Program.cs
+        // 1 - Program.cs
+        //          call methods
+        // 2 - Journal.cs
+        //          _entries : List<Entry>;
+        //          Add() : void
+        //          Display() : void
+        //          Save() : void
+        //          Load() : void
+        // 3 - Entry.cs
+        //          _date : string;
+        //          _prompt : string;
+        //          _response : string;
+        // 4 - Prompt.cs
+        //          GetRandomPrompt() : string;
+        //          
     }
 }
